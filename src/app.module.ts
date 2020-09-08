b1b4ccfc40795d5d152a729fa4f4e5dd3a587d1d
Bui Vongphachan <buitingtong@gmail.com>
2020-09-09 01:12:49 +0800

    connect to local database, create lesson resolver, service, entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,27 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { LessonModule } from './lesson/lesson.module';
+import { Lesson } from './lesson/lesson.entity';
 @Module({
   imports: [
-    GraphQLModule.forRoot({
-      autoSchemaFile: true
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: 'localhost',
+      port: 27017,
+      database: "c",
+      /* url:
+        'mongodb+srv://admin:123@cluster0.pice4.mongodb.net/lesson?retryWrites=true&w=majority', */
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson],
     }),
-    LessonModule
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    LessonModule,
   ],
   controllers: [],
   providers: [],
 })
 export class AppModule {}
- 
