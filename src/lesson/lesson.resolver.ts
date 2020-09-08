b6be533d1fcdde7a7e926508a690122c2a2deba6
Bui Vongphachan <buitingtong@gmail.com>
2020-09-09 01:30:37 +0800

    Validation Create Lesson Input

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,23 +1,20 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
 import { LessonService } from './lesson.service';
+import { CreateLessonInput } from './lesson.input';
 
 @Resolver(of => LessonType)
 export class LessonResolver {
   constructor(private lessonService: LessonService) {}
   @Query(returns => LessonType)
-  lesson(
-    @Args('id') id: string
-  ) {
-    return this.lessonService.getLesson(id)
+  lesson(@Args('id') id: string) {
+    return this.lessonService.getLesson(id);
   }
 
   @Mutation(returns => LessonType)
   createLesson(
-    @Args('name') name: string,
-    @Args('startDate') startDate: string,
-    @Args('endDate') endDate: string,
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
   ) {
-    return this.lessonService.createLesson(name, startDate, endDate);
+    return this.lessonService.createLesson(createLessonInput);
   }
 }
