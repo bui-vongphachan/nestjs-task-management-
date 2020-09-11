8457c5daea6ff3324d4323b6d231cd6775424491
Bui Vongphachan <buitingtong@gmail.com>
2020-09-11 23:22:03 +0800

    Setting up AuthModule, User Entity and UserRepository

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TasksController } from './tasks/tasks.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, AuthModule],
   controllers: [],
   providers: [],
 })
